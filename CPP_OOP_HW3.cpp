ef60d4a59277bf8e0b66b5c6f83099fbebd44f77
VLADIMIR <veselov2100@mail.ru>
2021-09-06 16:22:44 +0300

    Task 3 complete

--- CPP_OOP_HW3.cpp
+++ CPP_OOP_HW3.cpp
@@ -113,6 +113,116 @@ public:
 
 //Task 3 ======================================================================================================================
 
+class Fraction
+{
+private:
+	double numerator, denominator;
+public:
+	Fraction(double inputNumerator, double inputDenominator)
+		: numerator(inputNumerator), denominator(inputDenominator)
+	{
+		if (denominator == 0) denominator = 0.00000001;
+	}
+
+	friend Fraction operator+(const Fraction& d1, const Fraction& d2);
+	friend Fraction operator-(const Fraction& d1, const Fraction& d2);
+	friend Fraction operator*(const Fraction& d1, const Fraction& d2);
+	friend Fraction operator/(const Fraction& d1, const Fraction& d2);
+	friend Fraction operator-(const Fraction& d1);
+	friend std::ostream& operator<<(std::ostream& out, const Fraction& fraction);
+
+	void setDenominator(double inputDenominator)
+	{
+		if (inputDenominator == 0) inputDenominator = 0.00000001;
+		denominator = inputDenominator;
+	}
+
+	void setNumerator(double inputNumerator)
+	{
+		numerator = inputNumerator;
+	}
+
+	double getNumerator()
+	{
+		return numerator;
+	}
+
+	double getDenominator()
+	{
+		return denominator;
+	}
+
+};
+
+Fraction operator+(const Fraction& d1, const Fraction& d2)
+{
+	return Fraction(d1.numerator*d2.denominator + d2.numerator*d1.denominator, d1.denominator*d2.denominator);
+}
+
+Fraction operator-(const Fraction& d1, const Fraction& d2)
+{
+	return Fraction(d1.numerator * d2.denominator - d2.numerator * d1.denominator, d1.denominator * d2.denominator);
+}
+
+Fraction operator*(const Fraction& d1, const Fraction& d2)
+{
+	return Fraction(d1.numerator * d2.numerator, d1.denominator * d2.denominator);
+}
+
+Fraction operator/(const Fraction& d1, const Fraction& d2)
+{
+	return Fraction(d1.numerator * d2.denominator, d1.denominator * d2.numerator);
+}
+
+Fraction operator-(const Fraction& d1)
+{
+	return Fraction(-d1.numerator, -d1.denominator);
+}
+
+bool operator==(Fraction& d1, Fraction& d2)
+{
+	if ((d1.getNumerator() == d2.getNumerator()) && (d1.getDenominator() == d2.getDenominator())) return true;
+	else return false;
+}
+
+bool operator!=(Fraction& d1, Fraction& d2)
+{
+	if (d1 == d2) return false;
+	else return true;
+}
+
+bool operator>(Fraction& d1, Fraction& d2)
+{
+	if ((d1.getNumerator() / d1.getDenominator()) > (d2.getNumerator() / d2.getDenominator())) return true;
+	else return false;
+}
+
+bool operator<=(Fraction& d1, Fraction& d2)
+{
+	if (!(d1 > d2)) return true;
+	else return false;
+}
+
+bool operator<(Fraction& d1, Fraction& d2)
+{
+	if ((d1.getNumerator() / d1.getDenominator()) < (d2.getNumerator() / d2.getDenominator())) return true;
+	else return false;
+}
+
+bool operator>=(Fraction& d1, Fraction& d2)
+{
+	if (!(d1 < d2)) return true;
+	else return false;
+}
+
+
+std::ostream& operator<<(std::ostream& out, const Fraction& fraction)
+{
+	out << fraction.numerator << "/" << fraction.denominator;
+
+	return out;
+}
+
 int main()
 {
 	//Task 1 ==================================================================================================================
@@ -136,5 +246,18 @@ int main()
 	Minivan("Volkswagen", "Caravelle");
 
 	//Task 3 ==================================================================================================================
+	Fraction d1(3, 2);
+	Fraction d2(8, 3);
+	std::cout << "\n\n" << (d1+d2);
+	std::cout << "\n\n" << (d1 - d2);
+	std::cout << "\n\n" << (d1 * d2);
+	std::cout << "\n\n" << (d1 / d2);
+	std::cout << "\n\n" << (-d1) << "\n";
 
+	if (d1 == d2) std::cout << "\nFraction 1 == Fraction 2";
+	if (d1 != d2) std::cout << "\nFraction 1 != Fraction 2";
+	if (d1 > d2) std::cout << "\nFraction 1 > Fraction 2";
+	if (d1 <= d2) std::cout << "\nFraction 1 <= Fraction 2";
+	if (d1 < d2) std::cout << "\nFraction 1 < Fraction 2";
+	if (d1 >= d2) std::cout << "\nFraction 1 >= Fraction 2";
 	}
